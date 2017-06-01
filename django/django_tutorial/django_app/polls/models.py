850768da1a1713b27c0e21fe046177b10e23383e
bbungsang <821heejin@naver.com>
2017-06-01 15:30:18 +0900

    change models.py

--- django/django_tutorial/django_app/polls/models.py
+++ django/django_tutorial/django_app/polls/models.py
@@ -1,8 +1,23 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField('질문', max_length=200)
     pub_date = models.DateTimeField('발행일')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+    """
+    >>> from polls.models import *
+    >>> Question.objects.all()
+    <QuerySet [<Question: What's up?>]>
+    """
+    # 안 했을 시, <QuerySet []> 으로 표현된다.
 
 class Choice(models.Model):
     question = models.ForeignKey(
@@ -12,3 +27,7 @@ class Choice(models.Model):
     )
     choice_text = models.CharField('선택 내용', max_length=200)
     votes = models.IntegerField('총 투표수', default=0)
+
+    def __str__(self):
+        return self.choice_text
+
