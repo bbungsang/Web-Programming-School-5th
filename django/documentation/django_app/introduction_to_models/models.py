fdc1d7f42fc41a2c1561e099e2b539cc512bf09f
bbungsang <821heejin@naver.com>
2017-06-05 12:52:04 +0900

    Person 모델 수정(choices, recursive foreign key 추가), Car, Manufacturer 모델 추가

--- django/documentation/django_app/introduction_to_models/models.py
+++ django/documentation/django_app/introduction_to_models/models.py
@@ -7,6 +7,45 @@ class Person(models.Model):
         ('L', 'Large'),
 
     )
+    PERSON_TYPES = (
+        ('student', '학생'),
+        ('teacher', '선생'),
+    )
+
+    persontype = models.CharField(
+        '유형',
+        max_length=10,
+        choices=PERSON_TYPES,
+        default=PERSON_TYPES[0][0],
+    )
+    # teacher 속성 지정 (ForeignKey, 'self'를 이용해 자기 자신을 가리킬, null=Ture 허용)
+    teacher = models.ForeignKey('self',
+                                null=True,
+                                blank=True,
+                                on_delete=models.CASCADE
+                                )
     name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1)
+    shirt_size = models.CharField(
+        '셔츠사이즈',
+        max_length=1,
+        choices=SHIRT_SIZES,
+        help_text="약간 작게 나왔으니 참고해주세요."
+    )
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
 
