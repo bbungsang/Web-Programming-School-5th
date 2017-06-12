05c620b0c65c95eb30f2d1a241a5f89b9bbc59cf
bbungsang <821heejin@naver.com>
2017-06-12 14:18:20 +0900

    CommentLike 모델 추가

--- django/blog-upgrade/blog-upgrade/blog/models.py
+++ django/blog-upgrade/blog-upgrade/blog/models.py
@@ -1,46 +1,99 @@
-import re
-from django.forms import ValidationError
+# import re
+# from django.forms import ValidationError
 from django.db import models
+from django.contrib.auth.models import User
 
-from django.contrib.contenttypes.models import contenttype
-from django.contrib.contenttypes.fields import GenericForeignKey
 
-def lnglat_validator(value):
-    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
-        raise ValidationError('Invalid LngLat Type')
+# def lnglat_validator(value):
+#     if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+#         raise ValidationError('Invalid LngLat Type')
+
+# class User(models.Model):
+#     name = models.CharField(max_length=20)
+
 
 class Post(models.Model):
-    author = models.CharField(max_length=20)
-    title = models.CharField(
-        max_length=100,
-        verbose_name='제목',
-        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외',
-    ) # 길이 제한이 있는 문자열
-    content = models.TextField(verbose_name='내용') # 길이 제한이 없는 문자열
-    tags = models.CharField(max_length=100, blank=True)
-    acreated_at = models.DateTimeField(auto_now_add=True) # 최초 저장될 때 최초 저장 일시
-    updated_at = models.DateTimeField(auto_now=True) # 갱신 시 저장 일시
+    author = models.ForeignKey(User)  # 임포트한 User 사용
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # 최초 저장될 때 최초 저장 일시
+    modified_at = models.DateTimeField(auto_now=True)  # 갱신 시 저장 일시
+
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts',
+        through='PostLike',
+    )
+    tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        tag, tag_created = Tag.objects.get_or_create(
+            name=tag_name,
+        )  # get_or_create() : 뒤의 객체가 있으면 가져오고 없으면 만들어서 가져옴, 객체 생성 시 True 를 반환
+        if not self.tags.filter(name=tag_name).exists():
+            self.tags.add(tag)
+
+    def __str__(self):
+        hi = self.comment_set.all()
+        for hello in hi:
+            return '작성자 : {{ author }} \n 댓글 정보 : {{ comment_author }} : {{ comment_content }} \n 태그 정보 : {{ tag_info }}'.format(
+                author=self.author,
+                comment_author=hello.author,
+                comment_content=hello.content,
+                tag_info=self.tag_set().name,
+            )
+
+    @property
+    def like_count(self):
+        return self.like_users.count()
 
     class Meta:
         ordering = ['-id']
 
-    def __str__(self):
-        return self.title
 
 class Comment(models.Model):
+    author = models.ForeignKey(User)
     post = models.ForeignKey(Post)
     # 외래키로 필드를 생성하면 기입한 필드명이 아닌 '필드명_id' 의 이름으로 컬럼이 생성된다.
-    message = models.TextField()
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
-class LikeDislike(models.Model):
-    LIKE = 1
-    DISLIKE = -1
-
-    VOTES = (
-        (LIKE, 'Like')
-        (DISLIKE, 'Dislike')
+    like_users = models.ManyToManyField(
+        User,
+        through='CommentsLike',
+        related_name='like_comments',
     )
 
-    vote = models.SmallIntegerField(verbose_name=())
+
+class CommentsLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    created_date = models.DateField(auto_now_add=True)
+
+
+class PostLike(models.Model):
+    # LIKE = 1
+    # DISLIKE = -1
+    #
+    # VOTES = (
+    #     (LIKE, 'Like')
+    #     (DISLIKE, 'Dislike')
+    # )
+    #
+    # vote = models.SmallIntegerField(verbose_name=())
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'post_post_like_users'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Tag({})'.format(self.name)
